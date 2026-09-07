089957015ea3a3ba3aa559f4595d0a5832248f82
Mohamed <your-email@example.com>
2026-09-07 17:31:53 +0100

    angular size maxim

--- front/dist/pfe1/server/main.server.mjs
+++ front/dist/pfe1/server/main.server.mjs
@@ -1,10 +1,2 @@
 import './polyfills.server.mjs';
-import {
-  main_server_default
-} from "./chunk-CB4DZLXK.mjs";
-import "./chunk-Y7ZJQH3E.mjs";
-import "./chunk-24VIC3GD.mjs";
-export {
-  main_server_default as default
-};
-//# sourceMappingURL=main.server.mjs.map
+import{a}from"./chunk-4QGSFBZ5.mjs";import"./chunk-SEHFOHK3.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
